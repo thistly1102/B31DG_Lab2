464fa73c2a70816774450c3b18be3055700e9de7
thistly1102 <124710077+thistly1102@users.noreply.github.com>
2023-03-08 15:25:21 +0000

    Update main.c

--- main.c
+++ main.c
@@ -4,25 +4,28 @@
 void CycleF();
 
 //Define Input Pins on ESP32C
-#define T1_LED 19
-#define T2_INPUT 7
-#define T3_INPUT 2
-#define T4_INPUT 1
-#define T4_LED 0
+#define T1_LED 18
+#define T2_INPUT 0
+#define T3_INPUT 1
+#define T4_INPUT 2
+#define T4_LED 19
 
 Ticker Cycle; //Define Ticker 
 B31DGCyclicExecutiveMonitor monitor;  //Define Cyclic Executive Monitor
 
-int T2_Freq = 0;
-int T3_Freq = 0;
-int T4_counter = 0;
-int T2_NextCycle = 0;
+int T2_Freq = 0;  //Frequency Reading for Task 2
+int T3_Freq = 0;  //Frequency Reading for Task 3
+int T4_counter = 0; //Counter for Task 4
+int T2_NextCycle = 0; //Variable to Prevent Task 2 and Task 3 running in the same frame
+
+//Reading Values for Task 4
 double reading1 = 0;
 double reading2 = 0;
 double reading3 = 0;
 double reading4 = 0;
 double read_average;
-int counter = 0;
+
+int frame_counter = 0;  //frame counter for cyclic executive operation
 
 //B31DGMonitor monitor;
 
@@ -30,14 +33,14 @@ void setup(void)
 {
   Serial.begin(9600); // Starts the serial communication and sets baud rate to 9600
   
-  //Setup Pins
+  //Setup Input and LED Pins
   pinMode(T1_LED, OUTPUT);
   pinMode(T2_INPUT, INPUT);
   pinMode(T3_INPUT, INPUT);
   pinMode(T4_INPUT, INPUT);
   pinMode(T4_LED, OUTPUT); 
 
-  Cycle.attach(4000, CycleF); 
+  //Cycle.attach(4000, CycleF); //Ticker Interrupts every 4000us (4ms)
 
   monitor.startMonitoring(); // all the tasks should be released after this, this time can be queried using getTimeStart()
 }
@@ -45,13 +48,13 @@ void setup(void)
 void task1() {
    monitor.jobStarted(1);  // i between [1..5], i.e. 1 is digital output, 5 is serial print
    //Perform task1 - Signal Pulse
-   digitalWrite(T1_LED, HIGH);
-   delayMicroseconds(200);
-   digitalWrite(T1_LED, LOW);
-   delayMicroseconds(50);
-   digitalWrite(T1_LED, HIGH);
-   delayMicroseconds(30);
-   digitalWrite(T1_LED, LOW);
+   digitalWrite(T1_LED, HIGH);  //Set LED HIGH
+   delayMicroseconds(200);  //Delay 200us
+   digitalWrite(T1_LED, LOW);  //Set LED Low
+   delayMicroseconds(50); //Delay 50us
+   digitalWrite(T1_LED, HIGH);  //Set LED HIGH
+   delayMicroseconds(30); //Delay 50us  
+   digitalWrite(T1_LED, LOW); //Set LED LOW
    monitor.jobEnded(1);
 }
 
@@ -80,11 +83,11 @@ void task4() {
    {
      T4_counter = 0;
    }
-
-   if (T4_counter == 0)
-   {
-     reading1 = analogRead(T4_INPUT);  //Read Analog Signal
-   }
+  //Obtain Potentiometer Readings
+  if (T4_counter == 0)
+  {
+    reading1 = analogRead(T4_INPUT);  //Read Analog Signal
+  }
    else if (T4_counter == 1)
   {
     reading2 = analogRead(T4_INPUT);  //Read Analog Signal
@@ -99,78 +102,80 @@ void task4() {
   }
    read_average = (reading1 + reading2 + reading3 + reading4) / 4;  //Calculate average
 
-   if (read_average > 2047)
-   {
-     digitalWrite(T4_LED, HIGH);  //Error LED High
-   }
-   else
-   {
-     digitalWrite(T4_LED, LOW); //Error LED Low
-   }
-   T4_counter++;  //Increment Task 4 Counter
-   monitor.jobEnded(4);
+  if (read_average > 2047)
+  {
+    digitalWrite(T4_LED, HIGH);  //Error LED High
+  }
+  else
+  {
+    digitalWrite(T4_LED, LOW); //Error LED Low
+  }
+  Serial.printf("Average Reading = %d \n", read_average); // Display T2 and T3 Frequencies
+   
+  T4_counter++;  //Increment Task 4 Counter
+  monitor.jobEnded(4);
 }
 
 void task5() {
    monitor.jobStarted(5);
-   //Perform task5 - Display task2 and task3 frequencies
-   //Set 333Hz as minimum threshold
-  if(T2_Freq < 333)
-  {
-  T2_Freq = 333;
-  }
-  //Set 1000Hz as Maximum Threshold
-  else if(T2_Freq > 1000)
-  {
-    T2_Freq = 1000;
-  }
-  //Set 500Hz a Minimum Threshold
-  if(T3_Freq < 500)
-  {
-    T3_Freq = 500;
-  }
-  //Set 1000Hz as Maximum Threshold
-  else if(T3_Freq > 1000)
-  {
-    T3_Freq = 1000; 
-  }
-  T2_Freq = (T2_Freq - 333) * 0.127;  //Scaling to between 0 - 99
-  T3_Freq = (T3_Freq - 500) * 0.198;   //Scaling to between 0 - 99
-  Serial.printf("T2 Frequency = %d, T3 Frequency = %d \n", T2_Freq, T3_Freq); // print T2 and T3 Frequencies
-  monitor.jobEnded(5);
+   //Operate every 25 frames (100ms)
+   if(frame_counter % 25 == 0)
+   {
+     //Perform task5 - Display task2 and task3 frequencies
+    //Set 333Hz as minimum threshold
+     if(T2_Freq < 333)
+     {
+      T2_Freq = 333;
+      }
+      //Set 1000Hz as Maximum Threshold
+      else if(T2_Freq > 1000)
+      {
+       T2_Freq = 1000;
+      }
+      //Set 500Hz a Minimum Threshold
+      if(T3_Freq < 500)
+      {
+        T3_Freq = 500;
+      }
+      //Set 1000Hz as Maximum Threshold
+      else if(T3_Freq > 1000)
+      {
+        T3_Freq = 1000; 
+      }
+      T2_Freq = (T2_Freq - 333) * 0.127;  //Scaling to between 0 - 99
+      T3_Freq = (T3_Freq - 500) * 0.198;   //Scaling to between 0 - 99
+      Serial.printf("T2 Frequency = %d, T3 Frequency = %d \n", T2_Freq, T3_Freq); // print T2 and T3 Frequencies
+      monitor.jobEnded(5);
+   }
 }
 
 void CycleF()
 {
-  //Reset Frame Counter after highest common denominator 
-  if(counter > 50)
+  //200 frames is the smallest common denominator for the task periods
+  if(frame_counter > 199)
   {
-    counter = 0;
+    frame_counter = 0;
   }
-  task1();
-  if(counter % 2 == 0)
+  //Each Frame is 4ms
+  //Tasks are run in their respective frames
+  //Tasks are arranged such that the total run time within each frame does not exceed the frame length
+  //Task 1 is run every frame (4ms)
+  //Task 2 and Task 3 are never run in the same frame as the time taken would exceed the frame length
+  //Task 5 is called every 5 frames, however will only be run every 25 frames (100ms)
+  switch (frame_counter % 10)
   {
-    task3();
+    case 0: task1();            task3();  task4();  task5();  break;
+    case 1: task1();  task2();                                break; 
+    case 2: task1();            task3();                      break;
+    case 3: task1();                                          break;
+    case 4: task1();            task3();                      break;
+    case 5: task1();  task2();            task4();            break;
+    case 6: task1();            task3();                      break;
+    case 7: task1();                                          break;
+    case 8: task1();            task3();                      break;
+    case 9: task1();                                          break;
   }
-  if(counter % 5 ==0 && counter % 2 == 0)
-  {
-    T2_NextCycle = 1;
-  }
-  else if(T2_NextCycle = 1)
-  {
-    task2();
-    T2_NextCycle = 0;
-  }
-  if(counter % 5 == 0)
-  {
-    task4();
-  }
-  if(counter % 25 == 0)
-  {
-    task5();
-  }
-  counter++;
-
+  frame_counter++;  
 }
 
 // simple example of cyclic executive (version not using Ticker)
@@ -180,11 +185,11 @@ void loop(void)
   /*
   unsigned long bT = micros();
   for (int i=0; i<1; i++) {
-    task5();
+    task4();
   }
   unsigned long timeItTook = micros()-bT;
   Serial.print("Duration SerialOutput Job = ");
-  Serial.print(timeItTook/1);
+  Serial.print(timeItTook);
   exit(0);
   */
 }
